5d86272813123870492ba666abaa0409de158fa4
Diana <dianajohnson13@gmail.com>
2016-03-07 11:22:58 -0800

    plant-growth - added callback function to growthInDays()

--- misc/plant-growth.js
+++ misc/plant-growth.js
@@ -1,12 +1,12 @@
 function plantGrowth(species, inchesPerHourInDark, inchesPerHourInSun) {
-	return function growthInDays(days, availableDailySun) {
+	return function growthInDays(days, availableDailySun, logFunction) {
 
 		var dailyInchesGrownInSun = availableDailySun * inchesPerHourInSun;
 		var dailyInchesGrownInDark = (24 - availableDailySun) * inchesPerHourInDark;
 
 		var totalgrowth = (dailyInchesGrownInSun + dailyInchesGrownInDark) * days;
 
-		console.log("According to my calculations, if you give your " + species +
+		logFunction("According to my calculations, if you give your " + species +
 			" plant " + availableDailySun + " hours of sunlight per day over the course of " + days + 
 			" days, it will grow appoximately " + totalgrowth + " inches. Wow! It sure is growing fast!")
 	}
@@ -14,4 +14,4 @@ function plantGrowth(species, inchesPerHourInDark, inchesPerHourInSun) {
 
 var aloe = plantGrowth("aloe", 1, 3)
 
-aloe(4, 6)
+aloe(4, 6, console.log)
